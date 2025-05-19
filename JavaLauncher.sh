c7a9e3477272dd46bd7309ab3be9cc8b67bbd287
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2025-05-19 12:40:49 -0400

    Add reproducer

---
+++ JavaLauncher.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+java -jar "$DIR/../Java/classpath/io/qz/misc/OpenUriTest/1.0-SNAPSHOT/OpenUriTest-1.0-SNAPSHOT.jar"
